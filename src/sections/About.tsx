7f49f706ef49f73e00986d6be3e6e087502f1a20
_Andrz3j_ <154374052+Andrz3jj@users.noreply.github.com>
2026-04-26 12:03:19 +0200

    poprawienie responsywnosci

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -7,17 +7,18 @@ export default function About() {
         <>
             <div className="relative flex items-center justify-center h-screen overflow-hidden">
                 <div className="flex flex-col text-center">
-                    <span className="text-xl md:text-3xl font-extrabold pb-1 md:pb-2">Cześć!, Jestem Andrzej</span>
-                    <span className="sm:text-md md:text-xl font-semibold pb-0.5 md:pb-0.5">Uczeń, programista.</span>
-                    <span className="text-sm md:text-md ml-0.5 mr-0.5 md:ml-0 md:mr-0">
+                    <span className="text-xl md:text-4xl font-extrabold pb-1 md:pb-2">Cześć!, Jestem Andrzej</span>
+                    <span className="sm:text-md md:text-2xl font-semibold pb-0.5 md:pb-0.5">Uczeń, programista.</span>
+                    <span className="text-sm md:text-lg ml-0.5 mr-0.5 md:ml-0 md:mr-0">
                         Mam 17 lat i wolne chwile spędzam na kodowaniu.<br />
-                        Eksperymentuję z frontendem, uczę się nowych technologii<br />
+                        Eksperymentuję z frontendem,<br />
+                        uczę się nowych technologii<br />
                         i zmieniam pomysły w działające projekty.<br />
                     </span>
                     <div className="flex items-center justify-center space-x-2 pt-3">
-                        <a href="https://www.linkedin.com/in/andrzej-bajcarczyk-468941404/" target="_blank"><FaLinkedin size={30} /></a>
-                        <a href="https://github.com/Andrz3jj" target="_blank"><FaGithubSquare size={30} /></a>
-                        <a href="mailto:[andrzej1332@onet.pl]"><IoIosMail size={30} /></a>
+                        <a href="https://www.linkedin.com/in/andrzej-bajcarczyk-468941404/" target="_blank"><FaLinkedin size={35} /></a>
+                        <a href="https://github.com/Andrz3jj" target="_blank"><FaGithubSquare size={35} /></a>
+                        <a href="mailto:[andrzej1332@onet.pl]"><IoIosMail size={35} /></a>
                     </div>
                 </div>
             </div>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,9 +3,9 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 export default function Projects() {
     return (
         <>
-            <div className="relative flex items-center justify-center h-screen overflow-hidden">
-                <Tabs defaultValue="account" className="w-2/4">
-                    <TabsList className="flex w-max mx-auto mb-2">
+            <div className="relative flex items-center justify-center min-h-screen overflow-hidden">
+                <Tabs defaultValue="account" className="w-full md:w-2/4 ml-6 mr-6 md:ml-0 md:mr-0">
+                    <TabsList className="flex w-full mx-auto mb-2 overflow-x-auto">
                         <TabsTrigger value="work" className="text-lg">Praca</TabsTrigger>
                         <TabsTrigger value="education" className="text-lg">Edukacja</TabsTrigger>
                         <TabsTrigger value="competitions" className="text-lg">Konkursy</TabsTrigger>
@@ -14,7 +14,7 @@ export default function Projects() {
                     <TabsContent value="education" className="border-2 p-4 border-gray-200 rounded-lg">Edukacja</TabsContent>
                     <TabsContent value="competitions" className="border-2 p-4 border-gray-200 rounded-lg">Konkursy</TabsContent>
                 </Tabs>
-            </div >
+            </div>
         </>
     )
 }

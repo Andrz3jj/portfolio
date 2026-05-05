a9f35eaebb534a739932fe1fe16fe7564d929fbf
_Andrz3j_ <154374052+Andrz3jj@users.noreply.github.com>
2026-05-05 11:23:26 +0200

    usuniecie ikonki

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -8,7 +8,7 @@ export default function About() {
         <>
             <div className="relative flex items-center justify-center h-screen overflow-hidden">
                 <div className="flex flex-col text-center">
-                    <span className="text-xl md:text-4xl font-extrabold pb-1 md:pb-2">Cześć!, Jestem Andrzej 👋</span>
+                    <span className="text-xl md:text-4xl font-extrabold pb-1 md:pb-2">Cześć!, Jestem Andrzej</span>
                     <span className="sm:text-md md:text-xl font-semibold pb-0.5 md:pb-0.5 flex items-center justify-center gap-2">Web developer | Tarnobrzeg</span>
                     <span className="text-sm md:text-lg ml-0.5 mr-0.5 md:ml-0 md:mr-0">
                         Wolne chwile spędzam na kodowaniu.<br />
